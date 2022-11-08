81c9b25312dfd0e5fc0f967881f796382a868b51
onaufrago <asguth@outlook.com>
2022-11-08 15:04:40 -0300

    Update Conversor hora Am_Pm AV2 N3.py

--- Conversor hora Am_Pm AV2 N3.py
+++ Conversor hora Am_Pm AV2 N3.py
@@ -1,5 +1,6 @@
 import time
 import os
+import sys
 
 def clean():
     os.system("cls")
@@ -11,21 +12,31 @@ clean()
 
 hr = int(input("\nDigite a(s) hora(s): "))
 min = int(input("Digite o(s) minuto(s): "))
+A = ("AM")
+P = ("PM")
 
 def conversao(hr):
     return (hr - 12)
 
 def saida(hr,min):
+    if(hr > 24 or min > 59):
+        print("\nPrograma finalizado com a exceção SystemExit em (10 Segundos), (ERRO: Hr > 24 ou Min > 59)")
+        time.sleep(10)
+        sys.exit()
     
     print("\nHorário Convertido: ")
     
     if(min < 10):
         min = (f"0{min}")
     
+    if(hr == 0):
+        hr += 12
+    
     if(hr <= 12):
-        print(hr,":",min,"AM")
+        print(hr,":",min, A)
+    
     else:
-        print(conversao(hr),":", min, "PM")
+        print(conversao(hr),":", min, P)
 
 saida(hr, min)
 time.sleep(5)
